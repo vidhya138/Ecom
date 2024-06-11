12b79cd7e409ee9292a78e3c4976c53c2860ca48
vidhya138 <vidhs130801@gmail.com>
2024-06-11 16:15:30 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,15 @@
+// select side navbar
+var sidenav = document.querySelector(".side-navbar")
+
+
+function showNavbar()
+{
+    // sidenav.style.display="block"
+    sidenav.style.left="0"
+}
+
+function closeNavbar()
+{
+    // sidenav.style.display="none"
+    sidenav.style.left="-60%"
+}
